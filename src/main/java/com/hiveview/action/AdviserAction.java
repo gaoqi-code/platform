72effb741d6e6acd01bf4429f447eacef70731b4
luck1mail@126.com <123qweasd>
2017-03-11 20:42:44 +0800

    test

---
+++ src/main/java/com/hiveview/action/AdviserAction.java
@@ -0,0 +1,35 @@
+package com.hiveview.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/member/need")
+public class AdviserAction {
+
+	@RequestMapping(value="/list")
+	public ModelAndView list(HttpServletRequest request, ModelAndView mav) {
+		request.setAttribute("nav","center");
+		mav.setViewName("need/need_list");
+
+		return mav;
+	}
+
+	@RequestMapping(value="/add")
+	public ModelAndView add(HttpServletRequest request, ModelAndView mav) {
+		request.setAttribute("nav","center");
+		mav.setViewName("need/need_add");
+		return mav;
+	}
+
+	@RequestMapping(value="/update")
+	public ModelAndView update(HttpServletRequest request, ModelAndView mav) {
+		request.setAttribute("nav","center");
+		mav.setViewName("need/need_update");
+		return mav;
+	}
+
+}
