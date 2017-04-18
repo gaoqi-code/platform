28e3d0ee12b1493208c6f10994bc38426d9c7ed2
luck1mail@126.com <luck1mail@126.com>
2017-04-18 15:03:08 +0800

    图片验证码工具类。图片生成位置需需要改一下。

--- src/main/java/com/hiveview/util/ValidateCode.java
+++ src/main/java/com/hiveview/util/ValidateCode.java
@@ -1,188 +1,275 @@
 package com.hiveview.util;
 
+
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.Paint;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
 
-/**
- * 验证码生成器
- *
- */
-public class ValidateCode {
-    // 图片的宽度。
-    private int width = 120;
-    // 图片的高度。
-    private int height = 40;
-    // 验证码字符个数
-    private int codeCount = 4;
-    // 验证码干扰线数
-    private int lineCount = 50;
-    // 验证码
-    private String code = null;
-    // 验证码图片Buffer
-    private BufferedImage buffImg = null;
+public class ValidateCode{
 
-    private char[] codeSequence = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-            'I', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-            'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '8', '9' };
+    //使用到Algerian字体，系统里没有的话需要安装字体，字体只显示大写，去掉了1,0,i,o几个容易混淆的字符
+    public static final String VERIFY_CODES = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ";
+    private static Random random = new Random();
 
-    // 生成随机数
-    private Random random = new Random();
 
-    public ValidateCode() {
-        this.createCode();
+    /**
+     * 使用系统默认字符源生成验证码
+     * @param verifySize    验证码长度
+     * @return
+     */
+    public static String generateVerifyCode(int verifySize){
+        return generateVerifyCode(verifySize, VERIFY_CODES);
+    }
+    /**
+     * 使用指定源生成验证码
+     * @param verifySize    验证码长度
+     * @param sources   验证码字符源
+     * @return
+     */
+    public static String generateVerifyCode(int verifySize, String sources){
+        if(sources == null || sources.length() == 0){
+            sources = VERIFY_CODES;
+        }
+        int codesLen = sources.length();
+        Random rand = new Random(System.currentTimeMillis());
+        StringBuilder verifyCode = new StringBuilder(verifySize);
+        for(int i = 0; i < verifySize; i++){
+            verifyCode.append(sources.charAt(rand.nextInt(codesLen-1)));
+        }
+        return verifyCode.toString();
     }
 
     /**
-     *
-     * @param width
-     *            图片宽
-     * @param height
-     *            图片高
+     * 生成随机验证码文件,并返回验证码值
+     * @param w
+     * @param h
+     * @param outputFile
+     * @param verifySize
+     * @return
+     * @throws IOException
      */
-    public ValidateCode(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.createCode();
+    public static String outputVerifyImage(int w, int h, File outputFile, int verifySize) throws IOException{
+        String verifyCode = generateVerifyCode(verifySize);
+        outputImage(w, h, outputFile, verifyCode);
+        return verifyCode;
     }
 
     /**
-     *
-     * @param width
-     *            图片宽
-     * @param height
-     *            图片高
-     * @param codeCount
-     *            字符个数
-     * @param lineCount
-     *            干扰线条数
+     * 输出随机验证码图片流,并返回验证码值
+     * @param w
+     * @param h
+     * @param os
+     * @param verifySize
+     * @return
+     * @throws IOException
      */
-    public ValidateCode(int width, int height, int codeCount, int lineCount) {
-        this.width = width;
-        this.height = height;
-        this.codeCount = codeCount;
-        this.lineCount = lineCount;
-        this.createCode();
+    public static String outputVerifyImage(int w, int h, OutputStream os, int verifySize) throws IOException{
+        String verifyCode = generateVerifyCode(verifySize);
+        outputImage(w, h, os, verifyCode);
+        return verifyCode;
     }
 
-    public void createCode() {
-        int codeX = 0;
-        int fontHeight = 0;
-        fontHeight = height - 5;// 字体的高度
-        codeX = width / (codeCount+3);// 每个字符的宽度
-
-        // 图像buffer
-        buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = buffImg.createGraphics();
-
-        // 将图像填充为白色
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
-
-        // 创建字体
-        ImgFontByte imgFont = new ImgFontByte();
-        Font font = imgFont.getFont(fontHeight);
-        g.setFont(font);
-
-        // 绘制干扰线
-        for (int i = 0; i < lineCount; i++) {
-            int xs = getRandomNumber(width);
-            int ys = getRandomNumber(height);
-            int xe = xs + getRandomNumber(width / 8);
-            int ye = ys + getRandomNumber(height / 8);
-            g.setColor(getRandomColor());
-            g.drawLine(xs, ys, xe, ye);
+    /**
+     * 生成指定验证码图像文件
+     * @param w
+     * @param h
+     * @param outputFile
+     * @param code
+     * @throws IOException
+     */
+    public static void outputImage(int w, int h, File outputFile, String code) throws IOException{
+        if(outputFile == null){
+            return;
         }
-
-        StringBuffer randomCode = new StringBuffer();
-        // 随机产生验证码字符
-        for (int i = 0; i < codeCount; i++) {
-            String strRand = String.valueOf(codeSequence[random
-                    .nextInt(codeSequence.length)]);
-            // 设置字体颜色
-            g.setColor(getRandomColor());
-            // 设置字体位置
-            g.drawString(strRand, (i + 1) * codeX,
-                    getRandomNumber(height / 2) + 25);
-            randomCode.append(strRand);
+        File dir = outputFile.getParentFile();
+        if(!dir.exists()){
+            dir.mkdirs();
         }
-        code = randomCode.toString();
+        try{
+            outputFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            outputImage(w, h, fos, code);
+            fos.close();
+        } catch(IOException e){
+            throw e;
+        }
     }
 
-    /** 获取随机颜色 */
-    private Color getRandomColor() {
-        int r = getRandomNumber(255);
-        int g = getRandomNumber(255);
-        int b = getRandomNumber(255);
+    /**
+     * 输出指定验证码图片流
+     * @param w
+     * @param h
+     * @param os
+     * @param code
+     * @throws IOException
+     */
+    public static void outputImage(int w, int h, OutputStream os, String code) throws IOException{
+        int verifySize = code.length();
+        BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Random rand = new Random();
+        Graphics2D g2 = image.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        Color[] colors = new Color[5];
+        Color[] colorSpaces = new Color[] { Color.WHITE, Color.CYAN,
+                Color.GRAY, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE,
+                Color.PINK, Color.YELLOW };
+        float[] fractions = new float[colors.length];
+        for(int i = 0; i < colors.length; i++){
+            colors[i] = colorSpaces[rand.nextInt(colorSpaces.length)];
+            fractions[i] = rand.nextFloat();
+        }
+        Arrays.sort(fractions);
+
+        g2.setColor(Color.GRAY);// 设置边框色
+        g2.fillRect(0, 0, w, h);
+
+        Color c = getRandColor(200, 250);
+        g2.setColor(c);// 设置背景色
+        g2.fillRect(0, 2, w, h-4);
+
+        //绘制干扰线
+        Random random = new Random();
+        g2.setColor(getRandColor(160, 200));// 设置线条的颜色
+        for (int i = 0; i < 20; i++) {
+            int x = random.nextInt(w - 1);
+            int y = random.nextInt(h - 1);
+            int xl = random.nextInt(6) + 1;
+            int yl = random.nextInt(12) + 1;
+            g2.drawLine(x, y, x + xl + 40, y + yl + 20);
+        }
+
+        // 添加噪点
+        float yawpRate = 0.05f;// 噪声率
+        int area = (int) (yawpRate * w * h);
+        for (int i = 0; i < area; i++) {
+            int x = random.nextInt(w);
+            int y = random.nextInt(h);
+            int rgb = getRandomIntColor();
+            image.setRGB(x, y, rgb);
+        }
+
+        shear(g2, w, h, c);// 使图片扭曲
+
+        g2.setColor(getRandColor(100, 160));
+        int fontSize = h-4;
+        Font font = new Font("Algerian", Font.ITALIC, fontSize);
+        g2.setFont(font);
+        char[] chars = code.toCharArray();
+        for(int i = 0; i < verifySize; i++){
+            AffineTransform affine = new AffineTransform();
+            affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1), (w / verifySize) * i + fontSize/2, h/2);
+            g2.setTransform(affine);
+            g2.drawChars(chars, i, 1, ((w-10) / verifySize) * i + 5, h/2 + fontSize/2 - 10);
+        }
+
+        g2.dispose();
+        ImageIO.write(image, "jpg", os);
+    }
+
+    private static Color getRandColor(int fc, int bc) {
+        if (fc > 255)
+            fc = 255;
+        if (bc > 255)
+            bc = 255;
+        int r = fc + random.nextInt(bc - fc);
+        int g = fc + random.nextInt(bc - fc);
+        int b = fc + random.nextInt(bc - fc);
         return new Color(r, g, b);
     }
 
-    /** 获取随机数 */
-    private int getRandomNumber(int number) {
-        return random.nextInt(number);
+    private static int getRandomIntColor() {
+        int[] rgb = getRandomRgb();
+        int color = 0;
+        for (int c : rgb) {
+            color = color << 8;
+            color = color | c;
+        }
+        return color;
     }
 
-    public void write(String path) throws IOException {
-        OutputStream sos = new FileOutputStream(path);
-        this.write(sos);
+    private static int[] getRandomRgb() {
+        int[] rgb = new int[3];
+        for (int i = 0; i < 3; i++) {
+            rgb[i] = random.nextInt(255);
+        }
+        return rgb;
     }
 
-    public void write(OutputStream sos) throws IOException {
-        ImageIO.write(buffImg, "png", sos);
-        sos.close();
+    private static void shear(Graphics g, int w1, int h1, Color color) {
+        shearX(g, w1, h1, color);
+        shearY(g, w1, h1, color);
     }
 
-    public BufferedImage getBuffImg() {
-        return buffImg;
-    }
+    private static void shearX(Graphics g, int w1, int h1, Color color) {
 
-    public String getCode() {
-        return code;
-    }
+        int period = random.nextInt(2);
 
-    /** 字体样式类 */
-    class ImgFontByte {
-        public Font getFont(int fontHeight) {
-            try {
-                Font baseFont = Font.createFont(Font.TRUETYPE_FONT,
-                        new ByteArrayInputStream(hex2byte(getFontByteStr())));
-                return baseFont.deriveFont(Font.PLAIN, fontHeight);
-            } catch (Exception e) {
-                return new Font("Arial", Font.PLAIN, fontHeight);
+        boolean borderGap = true;
+        int frames = 1;
+        int phase = random.nextInt(2);
+
+        for (int i = 0; i < h1; i++) {
+            double d = (double) (period >> 1)
+                    * Math.sin((double) i / (double) period
+                    + (6.2831853071795862D * (double) phase)
+                    / (double) frames);
+            g.copyArea(0, i, w1, 1, (int) d, 0);
+            if (borderGap) {
+                g.setColor(color);
+                g.drawLine((int) d, i, 0, i);
+                g.drawLine((int) d + w1, i, w1, i);
             }
         }
 
-        private byte[] hex2byte(String str) {
-            if (str == null)
-                return null;
-            str = str.trim();
-            int len = str.length();
-            if (len == 0 || len % 2 == 1)
-                return null;
+    }
 
-            byte[] b = new byte[len / 2];
-            try {
-                for (int i = 0; i < str.length(); i += 2) {
-                    b[i / 2] = (byte) Integer.decode(
-                            "0x" + str.substring(i, i + 2)).intValue();
-                }
-                return b;
-            } catch (Exception e) {
-                return null;
+    private static void shearY(Graphics g, int w1, int h1, Color color) {
+
+        int period = random.nextInt(40) + 10; // 50;
+
+        boolean borderGap = true;
+        int frames = 20;
+        int phase = 7;
+        for (int i = 0; i < w1; i++) {
+            double d = (double) (period >> 1)
+                    * Math.sin((double) i / (double) period
+                    + (6.2831853071795862D * (double) phase)
+                    / (double) frames);
+            g.copyArea(i, 0, 1, h1, 0, (int) d);
+            if (borderGap) {
+                g.setColor(color);
+                g.drawLine(i, (int) d, i, 0);
+                g.drawLine(i, (int) d + h1, i, h1);
             }
+
         }
 
-        // 字体文件的十六进制字符串
-        private String getFontByteStr() {
-return "";
-        }
-  }
+    }
+    public static void main(String[] args) throws IOException{
+        File dir = new File("F:/verifies");
+        int w = 200, h = 80;
+//        for(int i = 0; i < 50; i++){
+//            String verifyCode = generateVerifyCode(4);
+//            File file = new File(dir, verifyCode + ".jpg");
+//            outputImage(w, h, file, verifyCode);
+//        }
+
+        String verifyCode = generateVerifyCode(4);
+        File file = new File(dir, verifyCode + ".jpg");
+        outputImage(w, h, file, verifyCode);
+    }
 }
